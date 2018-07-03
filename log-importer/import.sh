cb3fb515c61a3af913dedb3a3bd8d30ea91194eb
Uwe Schindler <uwe@thetaphi.de>
2018-07-03 15:32:57 +0200

    Update the JQ script to recreate index and send stuff in chunks of 10000 datasets

--- log-importer/import.sh
+++ log-importer/import.sh
@@ -7,10 +7,19 @@ SERVER=http://localhost:9200
 INDEX=pangaea-recommender
 TYPE=rec
 
-# Tools config (add your install dir, if not in path)
-JQ=./jq.exe
+set +e
 
+echo "Deleting old index (ignore errors)..."
 curl -s -XDELETE "$SERVER/$INDEX"
-curl -s -XPUT "$SERVER/$INDEX" --data-binary @indexconfig.json
+echo " -- DONE"
 
-cat $FILE | $JQ --compact-output 'to_entries[] | {"index": { "_id" : .key }}, .value' | curl -s -XPOST "$SERVER/$INDEX/$TYPE/_bulk" --data-binary @-
+set -e
+
+echo "Recreate index..."
+curl -s -XPUT "$SERVER/$INDEX" --data-binary @indexconfig.json
+echo " -- DONE"
+
+echo "Importing recommender data..."
+cat $FILE | jq --compact-output 'to_entries[] | {"index": { "_id" : .key }}, .value' \
+  | split --lines=20000 --numeric-suffixes --filter="echo -n 'Importing '\$FILE'...'; curl -s -XPOST '$SERVER/$INDEX/$TYPE/_bulk' --data-binary @- >/dev/null; echo ' - DONE'" - 'chunk'
+echo "IMPORT FINSIHED."
